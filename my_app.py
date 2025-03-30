85beb4cbd4355df41837c128699f50b53703782f
Neiro8888 <hloperartem@yandex.com>
2025-03-30 13:03:19 +0500

    YEEE

--- my_app.py
+++ my_app.py
@@ -1,36 +1,49 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QLabel, QHBoxLayout, QVBoxLayout
+import sys
+import os
 import random
 app = QApplication([])
 main_win = QWidget()
-main_win.setWindowTitle('Бабка овера')
-main_win.resize(400,200)
-
+main_win.setWindowTitle('132')
+main_win.resize(600,300)
+cmd = 'python second_win.py'
+def start2():
+    main_win.hide()
+    os.system(cmd)
+    
+    
 linev =  QVBoxLayout()
 line1 =  QHBoxLayout()
 line2 =  QHBoxLayout()
 line3 =  QHBoxLayout()
+line4 =  QHBoxLayout()
 
 text = QLabel("Добро пожаловать в программу по определению состояния здоровья")
 
 
 
-text2 = QLabel("Данное приложение позвалит вам с помощью теста Руфье провести первичную диагностику вашего здоровья.")
+text2 = QLabel("Данное приложение позволит вам с помощью теста Руфье провести первичную диагностику вашего здоровья.")
 
 
 
-text4 = QLabel("Проба Руфье представляет собой нагрузочный комплекс, предназначенный для оценки работоспособости средца при физический нагрузке. \n У испытуе")
+text4 = QLabel("Проба Руфье представляет собой нагрузочный комплекс, предназначенный для оценки работоспособости средца при физический нагрузке. \nУ испытуемого, находящегося в положениилежа на спине в течении 5 мин, определяют частоту пульса за 15 секунд; \nзатем в течении 45 секунд испытуемый выполняет 30 приседаний.\nПосле окончания нагрузки испытуемый ложится, и у него вновь подсчитывается число пульсация за первые 15 секунд, \n а потом - за последние 15 секунд первой минуты периода восстановления. \nВажно! Если в процессе проведения испытания вы почувствуете себя плохо ( появится головокружение, шум в\nушах, сильная отдышка и др.) то тест необходимо прервать и обратиться к врачу.")
 
-text5 = QLabel("C++")
+
 line1.addWidget(text, alignment=Qt.AlignCenter)
 
 line2.addWidget(text2, alignment=Qt.AlignCenter)
 
 line3.addWidget(text4, alignment=Qt.AlignCenter)
-line3.addWidget(text5, alignment=Qt.AlignCenter)
+button = QPushButton("Начать")
+button.clicked.connect(start2)
+button.text()
+line4.addWidget(button, alignment=Qt.AlignCenter)
+
 linev.addLayout(line1)
 linev.addLayout(line2)
 linev.addLayout(line3)
+linev.addLayout(line4)
 
 main_win.setLayout(linev)
 main_win.show()
--- second_win.py
+++ second_win.py
@@ -1,1 +1,58 @@
-# напиши здесь код для второго экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QLabel, QHBoxLayout, QVBoxLayout, QLineEdit
+import sys
+import os
+import random
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('Здоровье')
+main_win.resize(600,300)    
+linev =  QVBoxLayout()
+line1 =  QVBoxLayout()
+line2 =  QVBoxLayout()
+line3 =  QVBoxLayout()
+line4 =  QVBoxLayout()
+
+text = QLabel("Введите Ф.И.О")
+pole1 = QLineEdit()
+text1 = QLabel('Кол-во полных лет')
+pole2 = QLineEdit()
+text2 = QLabel('Лягте на спину и замерьте пульс за 15 секунд. Нажмите кнопку "Начать первый тест", чтобы запустить таймер.\nРезультат запишите в соответствующее поле.')
+button = QPushButton('Начать первый тест')
+pole3 = QLineEdit()
+text3 = QLabel('Выполните 30 приседаний за 45 секунд. Для этого нажмите кнопку "Начать делать приседания",\nчтобы запустить счетчик приседаний.')
+button2 = QPushButton('Начать делать приседания')
+text4 = QLabel('Лягте на спину и замерьте пульс сначала за первые 15 секунд минуты, затем за последние 15 секунд.\nНажмите кнопку "Начать финальный тест", чтобы запустить таймер.\nЗелёным обозначены секунды, в течение которых необходимо \nпроводить измерения, черным - секунды без замера пульсаций. Результаты запишите в соответствующие поля.'
+)
+button3 = QPushButton('Начать финальный тест')
+pole4 = QLineEdit()
+pole5 = QLineEdit()
+t = QLabel('00:00:15')
+line1.addWidget(text, alignment=Qt.AlignLeft)
+line1.addWidget(pole1, alignment=Qt.AlignLeft)
+line1.addWidget(text1, alignment=Qt.AlignLeft)
+line1.addWidget(pole2, alignment=Qt.AlignLeft)
+line1.addWidget(text2, alignment=Qt.AlignLeft)
+line1.addWidget(button, alignment=Qt.AlignLeft)
+line2.addWidget(t,alignment=Qt.AlignCenter)
+line1.addWidget(pole3, alignment=Qt.AlignLeft)
+line1.addWidget(text3, alignment=Qt.AlignLeft)
+line1.addWidget(button2, alignment=Qt.AlignLeft)
+line1.addWidget(text4, alignment=Qt.AlignLeft)
+line1.addWidget(button3, alignment=Qt.AlignLeft)
+line1.addWidget(pole4, alignment=Qt.AlignLeft)
+line1.addWidget(pole5, alignment=Qt.AlignLeft)
+
+button = QPushButton("Отправить результаты")
+
+button.text()
+line4.addWidget(button, alignment=Qt.AlignCenter)
+
+linev.addLayout(line1)
+linev.addLayout(line2)
+linev.addLayout(line3)
+linev.addLayout(line4)
+
+main_win.setLayout(linev)
+main_win.show()
+app.exec_()
