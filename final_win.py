34292f4f549db9c632950cb2fa0c2dcc58162267
Neiro8888 <hloperartem@yandex.com>
2025-03-30 13:07:21 +0500

    ye

--- final_win.py
+++ final_win.py
@@ -1,1 +1,31 @@
-# напиши здесь код третьего экрана приложения
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QLabel, QHBoxLayout, QVBoxLayout, QLineEdit
+import sys
+import os
+import random
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle('Здоровье')
+main_win.resize(600,300)    
+linev =  QVBoxLayout()
+line1 =  QVBoxLayout()
+line2 =  QVBoxLayout()
+line3 =  QVBoxLayout()
+line4 =  QVBoxLayout()
+
+text = QLabel("Индекс Руфье:")
+t = QLabel('Работоспособность сердца:')
+line1.addWidget(text, alignment=Qt.AlignCenter)
+line1.addWidget(t, alignment=Qt.AlignCenter)
+
+
+
+
+linev.addLayout(line1)
+linev.addLayout(line2)
+linev.addLayout(line3)
+linev.addLayout(line4)
+
+main_win.setLayout(linev)
+main_win.show()
+app.exec_()
