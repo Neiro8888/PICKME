2dee5c9349a172972e3e11396c37f80c32898da0
Neiro8888 <hloperartem@yandex.com>
2025-04-13 13:11:00 +0500

    finish

--- final_win.py
+++ final_win.py
@@ -12,13 +12,16 @@ line1 =  QVBoxLayout()
 line2 =  QVBoxLayout()
 line3 =  QVBoxLayout()
 line4 =  QVBoxLayout()
+b = ''
 with open('file.txt', 'r') as r:
-    a = r.read()
+    a = int(r.read())
     r.close()
 with open('age.txt', 'r') as r:
-    age = r.read()
+    age = int(r.read())
     r.close()
-def workers(age):
+b = ''
+def workers(age,a):
+    
     low = 21
     nice = 17
     nice1 = 20.9
@@ -27,28 +30,112 @@ def workers(age):
     hight_middle = 6.5
     hight_middle1 = 11.9
     hight = 6.4
-
-    if age == 7 or age == 8:
+    
+    if int(age) == 7 or int(age) == 8:
+        
         if a >= low:
             b = ' Низкая '
         elif a >= nice and a <= nice1:
-            b = 'Удовлетворительная'
+            b = ' Удовлетворительная'
         elif a >= middle and a <= middle1:
-            b = 'Средняя'
+            b = ' Средняя'
         elif a >= hight_middle and a <= hight_middle1:
-            b = 'Выше среднего'
+            b = ' Выше среднего'
         elif a <= hight :
-            b = 'Выше среднего'
+            b = ' Высокая'
+    if int(age) == 9 or int(age) == 10:
+        
+        low = 19.5
+        nice = 15.5
+        nice1 = 19.4
+        middle = 10.5
+        middle1 = 15.4
+        hight_middle = 5
+        hight_middle1 = 10.4
+        hight = 4.9
+        if a >= low:
+            b = ' Низкая '
+        elif a >= nice and a <= nice1:
+            b = ' Удовлетворительная'
+        elif a >= middle and a <= middle1:
+            b = ' Средняя'
+        elif a >= hight_middle and a <= hight_middle1:
+            b = ' Выше среднего'
+        elif a <= hight :
+            b = ' Высокая'
+    if int(age) == 11 or int(age) == 12:
+        
+        low = 19.5 - 1.5
+        nice = 15.5 - 1.5
+        nice1 = 19.4 - 1.5
+        middle = 10.5 - 1.5
+        middle1 = 15.4 - 1.5
+        hight_middle = 5 - 1.5
+        hight_middle1 = 10.4 - 1.5
+        hight = 4.9 - 1.5
+        if a >= low:
+            b = ' Низкая '
+        elif a >= nice and a <= nice1:
+            b = ' Удовлетворительная'
+        elif a >= middle and a <= middle1:
+            b = ' Средняя'
+        elif a >= hight_middle and a <= hight_middle1:
+            b = ' Выше среднего'
+        elif a <= hight :
+            b = ' Высокая'
+    if int(age) == 13 or int(age) == 14:
+        
+        low = 19.5 - 1.5 - 1.5
+        nice = 15.5 - 1.5 - 1.5
+        nice1 = 19.4 - 1.5 - 1.5 
+        middle = 10.5 - 1.5- 1.5
+        middle1 = 15.4 - 1.5- 1.5
+        hight_middle = 5 - 1.5- 1.5
+        hight_middle1 = 10.4 - 1.5- 1.5
+        hight = 4.9 - 1.5- 1.5
+        if a >= low:
+            b = ' Низкая '
+        elif a >= nice and a <= nice1:
+            b = ' Удовлетворительная'
+        elif a >= middle and a <= middle1:
+            b = ' Средняя'
+        elif a >= hight_middle and a <= hight_middle1:
+            b = ' Выше среднего'
+        elif a <= hight :
+            b = ' Высокая'
+    if int(age) >= 15:
+        
+        low = 19.5 - 1.5 - 1.5- 1.5
+        nice = 15.5 - 1.5 - 1.5- 1.5
+        nice1 = 19.4 - 1.5 - 1.5 - 1.5
+        middle = 10.5 - 1.5- 1.5- 1.5
+        middle1 = 15.4 - 1.5- 1.5- 1.5
+        hight_middle = 5 - 1.5- 1.5- 1.5
+        hight_middle1 = 10.4 - 1.5- 1.5- 1.5
+        hight = 4.9 - 1.5- 1.5- 1.5
+        if a >= low:
+            b = ' Низкая '
+        elif a >= nice and a <= nice1:
+            b = ' Удовлетворительная'
+        elif a >= middle and a <= middle1:
+            b = ' Средняя'
+        elif a >= hight_middle and a <= hight_middle1:
+            b = ' Выше среднего'
+        elif a <= hight :
+            b = ' Высокая'
+    
+    
+    t = QLabel('Работоспособность сердца:' + str(b))
+    line1.addWidget(t, alignment=Qt.AlignCenter)
 
 
+text = QLabel('Индекс Руфье: '+ str(a))
 
-
-    return b        
-
-text = QLabel('Индекс Руфье:' + a)
-t = QLabel('Работоспособность сердца:')
 line1.addWidget(text, alignment=Qt.AlignCenter)
-line1.addWidget(t, alignment=Qt.AlignCenter)
+          
+age1 = workers(age,a)
+
+
 
 
 
--- second_win.py
+++ second_win.py
@@ -3,13 +3,20 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QLabel, QHBoxLayo
 import sys
 import os
 import random
+
+from datetime import datetime, timedelta
+import threading
+import time
+
+
+
 app = QApplication([])
 main_win = QWidget()
 main_win.setWindowTitle('Здоровье')
-main_win.resize(600,300)    
+main_win.resize(700,300)    
 linev =  QVBoxLayout()
 line1 =  QVBoxLayout()
-line2 =  QVBoxLayout()
+line2 =  QHBoxLayout()
 line3 =  QVBoxLayout()
 line4 =  QVBoxLayout()
 cmd = 'python final_win.py'
@@ -27,12 +34,27 @@ def start2():
     file432.close()
     main_win.hide()
     os.system(cmd)
+def my_timer(print_interval):
+    data = threading.local()
+    data.counter = 14
+    while data.counter >= 0:
+        time.sleep(print_interval)
+        if data.counter >= 10:
+            tex.setText('00:00:'+str(data.counter))
+        if data.counter < 10:
+            tex.setText('00:00:0'+str(data.counter))
+        data.counter -= 1
+    tex.setText('00:00:00')
+def you():
+    t = threading.Thread(target=my_timer, name="My time thread", args=(1, ), daemon=True)
+    t.start()
 text = QLabel("Введите Ф.И.О")
 pole1 = QLineEdit()
 text1 = QLabel('Кол-во полных лет')
 pole2 = QLineEdit()
 text2 = QLabel('Лягте на спину и замерьте пульс за 15 секунд. Нажмите кнопку "Начать первый тест", чтобы запустить таймер.\nРезультат запишите в соответствующее поле.')
 button = QPushButton('Начать первый тест')
+
 pole3 = QLineEdit()
 text3 = QLabel('Выполните 30 приседаний за 45 секунд. Для этого нажмите кнопку "Начать делать приседания",\nчтобы запустить счетчик приседаний.')
 button2 = QPushButton('Начать делать приседания')
@@ -41,14 +63,16 @@ text4 = QLabel('Лягте на спину и замерьте пульс сна
 button3 = QPushButton('Начать финальный тест')
 pole4 = QLineEdit()
 pole5 = QLineEdit()
-t = QLabel('00:00:15')
+tex = QLabel('00:00:15')
+button.clicked.connect(you)
 line1.addWidget(text, alignment=Qt.AlignLeft)
+line2.addWidget(tex,alignment=Qt.AlignRight)
 line1.addWidget(pole1, alignment=Qt.AlignLeft)
 line1.addWidget(text1, alignment=Qt.AlignLeft)
 line1.addWidget(pole2, alignment=Qt.AlignLeft)
 line1.addWidget(text2, alignment=Qt.AlignLeft)
 line1.addWidget(button, alignment=Qt.AlignLeft)
-line2.addWidget(t,alignment=Qt.AlignCenter)
+
 line1.addWidget(pole3, alignment=Qt.AlignLeft)
 line1.addWidget(text3, alignment=Qt.AlignLeft)
 line1.addWidget(button2, alignment=Qt.AlignLeft)
